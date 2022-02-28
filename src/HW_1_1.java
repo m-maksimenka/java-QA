0d4fdfc7e1b758810f65c1439de11dbee441b493
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-02-28 22:49:07 +0300

    add HW_1_1.java

---
+++ src/HW_1_1.java
@@ -0,0 +1,10 @@
+// Homework 1: https://docs.google.com/document/d/1NOaWwb9rSApKqPI4AYsbRdjt5t3CxC1yIo2ZwobpEXA/edit
+
+public class HW_1_1 {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+    }
+}
