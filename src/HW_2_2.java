7ad5aa87bf15511288b15b1c1e4f623ed9f2741b
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-03-05 22:32:45 +0300

    add HW_2_2

---
+++ src/HW_2_2.java
@@ -0,0 +1,8 @@
+// https://docs.google.com/document/d/1sjgtl1dIcmbgezc-Z7tCCm3vFj5uSlkYX522O0dV_mE/edit
+
+public class HW_2_2 {
+    public static void main(String[] args) {
+        String smile = "\u263A";
+        System.out.println(smile);
+    }
+}
