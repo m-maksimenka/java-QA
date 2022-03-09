ceef09ceb1c2f3532e2939c9bd2dd5e984fa693d
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-03-09 20:39:09 +0300

    add HW_3_1_1

---
+++ src/HW_3_1_1.java
@@ -0,0 +1,15 @@
+// https://docs.google.com/document/d/1eBqX9c6cHKRJwrrh5L-QRj6ka6nVqDw3uMvdLb-cLOk/edit
+
+public class HW_3_1_1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 8;
+        if (a == b) {
+            System.out.println("a == b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else if (a > b) {
+            System.out.println("a > b");
+        }
+    }
+}
