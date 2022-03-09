92703d20c44d3e78eb31675508d4f2391600ef9f
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-03-09 20:46:14 +0300

    add HW_3_1_2

---
+++ src/HW_3_1_2.java
@@ -0,0 +1,13 @@
+// https://docs.google.com/document/d/1eBqX9c6cHKRJwrrh5L-QRj6ka6nVqDw3uMvdLb-cLOk/edit
+
+public class HW_3_1_2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 8;
+        if ((a + b) % 2 == 0) {
+            System.out.println("maybe a and b are even");
+        } else if ((a + b) % 2 == 1) {
+            System.out.println("some variable is odd");
+        }
+    }
+}
