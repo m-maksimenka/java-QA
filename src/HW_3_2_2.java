68238033f52faaaf286654559d130303b8a8ff6f
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-03-10 18:56:08 +0300

    add HW_3_2_2

---
+++ src/HW_3_2_2.java
@@ -0,0 +1,9 @@
+// https://docs.google.com/document/d/1o6HaChpDACWK2NJphMSas--39vusfYlJ3QDsc709j8I/edit
+
+public class HW_3_2_2 {
+    public static void main(String[] args) {
+        for (int i = 5; i < 1000; i *= 5) {
+            System.out.println(i);
+        }
+    }
+}
